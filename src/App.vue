dee3d29cf87dfa070c23585d6698e9eb22374c01
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-07-06 15:56:58 +0200

    header terminato

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
-    
+    <MyHeader/>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+ import MyHeader from './components/MyHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    MyHeader
   }
 }
 </script>
 
 <style lang="scss">
-
+// importo gli stili da common
+@import './style/common'
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,117 @@
+<template>
+   <header class="container flex">
+       <div class="logo">
+           <img src="../assets/img/dc-logo.png" alt="DC logo">
+       </div>
+        <!-- nav -->
+           <nav>
+               <ul class="header-menu"> 
+                   <li v-for="link, index in menuLinks" :key="index" :class="{'current' : link.currentElement == true}">
+                       <a :href="link.url" :class="{'current' : link.currentElement == true}">{{link.name}} </a>
+                   </li>
+               </ul>
+           </nav>
+   </header>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+    data(){
+       return {
+           menuLinks: [
+               {
+                name: 'CHARACTERS',
+                url: '#',
+                currentElement: false,
+               },
+               {
+                name: 'COMICS',
+                url: '#',
+                currentElement: true,
+               },
+               {
+                name: 'MOVIES',
+                url: '#',
+                currentElement: false,
+               },
+               {
+                name: 'TV',
+                url: '#',
+                currentElement: false,
+               },
+               {
+                name: 'GAMES',
+                url: '#',
+                currentElement: false,
+               },
+               {
+                name: 'COLLECTIBLES',
+                url: '#',
+                currentElement: false,
+               },
+               {
+                name: 'VIDEOS',
+                url: '#',
+                currentElement: false,
+               },
+               {
+                name: 'FANS',
+                url: '#',
+                currentElement: false,
+               },
+               {
+                name: 'NEWS',
+                href: '#',
+                currentElement: false,
+               },
+               {
+                name: 'SHOP',
+                url: '#',
+                currentElement: false,
+               },
+
+           ]
+           
+
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables';
+@import '../style/common';
+header {
+    height: 80px;
+    padding: 20px 0;
+    align-items: center;
+    justify-content: space-between;
+    .logo{
+        img{
+            width: 50px;
+            display: inline;
+        }
+    }
+
+    .header-menu{
+        display: flex;
+        color: $main_color;
+        
+        li{
+            padding: 0 10px;
+            font-size: 10px;
+            a{
+                color: black;
+                font-weight: 700;
+                line-height: 80px;
+            }
+            a.current{
+                color: $main_color;
+            }
+        }
+    }
+
+}
+</style>
